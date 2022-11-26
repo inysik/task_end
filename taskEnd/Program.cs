f72a9a70eed4f9a60d4f5958c0457087e3153d8f
inysik <107579107+inysik@users.noreply.github.com>
2022-11-26 17:00:50 +0300

    Add files via upload

---
+++ taskEnd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string[] words = new string[] { "jojj", "tytrr", "kk", "kok", "op" };
+
+
+        for (byte i = 0; i < words.Length; i++)
+        {
+
+            if (words[i].Length <= 3)
+            {
+                // Console.Write("[");
+                System.Console.Write($"['{words[i]}']", words[i]);
+
+                // Console.Write("]");
+            }
+
+        }
+
+    }
+
+}
